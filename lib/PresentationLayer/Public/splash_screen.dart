c63db1ef58978996f71e5da530aff03b06823940
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-14 02:48:48 -0700

    Use dark theme

--- lib/PresentationLayer/Public/splash_screen.dart
+++ lib/PresentationLayer/Public/splash_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Center(
+            child: Text(
+          'اختبار التطبيق',
+          style: UITextStyle.title,
+        )),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:one_restaurant_delivery/Constants/get_pages.dart';
+import 'package:one_restaurant_delivery/Constants/ui_themes.dart';
 
 void main() async {
   await GetStorage.init();
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       fallbackLocale: const Locale('en'),
+      theme: darkTheme,
       title: 'One Restaurant Delivery',
       getPages: getPages,
     );
