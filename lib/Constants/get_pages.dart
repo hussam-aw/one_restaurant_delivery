d8ed7397bac789934938aacd6298bd8f823c7c05
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-15 05:39:17 -0700

    Add home get page

--- lib/Constants/get_pages.dart
+++ lib/Constants/get_pages.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 import 'package:one_restaurant_delivery/Constants/get_routes.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Public/home_screen.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Public/splash_screen.dart';
 
 List<GetPage<dynamic>> getPages = [
   GetPage(
-    name: AppRoutes.splashscreen,
+    name: AppRoutes.splashScreen,
     page: () => SplashScreen(),
+  ),
+  GetPage(
+    name: AppRoutes.homeScreen,
+    page: () => HomeScreen(),
   ),
 ];
