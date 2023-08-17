398a66b0d6c3a0dd469db7bebeccdf8bc2f190bd
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-17 07:22:14 -0700

    Create app icon button widget

---
+++ lib/PresentationLayer/Widgets/Public/ord_icon_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
+import 'package:one_restaurant_delivery/Constants/ui_styles.dart';
+
+class OrdIconButton extends StatelessWidget {
+  const OrdIconButton({
+    super.key,
+    required this.icon,
+    this.backgroundColor = UIColors.white,
+    this.borderRadius = raduis12,
+    required this.onPressed,
+  });
+
+  final Icon icon;
+  final Color backgroundColor;
+  final BorderRadius borderRadius;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: borderRadius,
+        border: Border.all(
+          color: UIColors.veryDarkGray,
+        ),
+      ),
+      child: Center(
+        child: IconButton(onPressed: onPressed, icon: icon),
+      ),
+    );
+  }
+}
