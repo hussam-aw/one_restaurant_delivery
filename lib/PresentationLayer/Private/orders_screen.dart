d100d5a6e889b15b79ff5e67796d2635c6c17adc
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-17 07:19:59 -0700

    Create orders screen

---
+++ lib/PresentationLayer/Private/orders_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Order/order_box.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_appbar.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: ordAppBar(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          child: Column(
+            children: [
+              spacerHeight(height: 25),
+              const PageTitle(title: 'الطلبات'),
+              spacerHeight(),
+              Expanded(
+                  child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return OrderBox();
+                },
+                separatorBuilder: (context, index) => spacerHeight(),
+                itemCount: 5,
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
