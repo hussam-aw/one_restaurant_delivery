1e509e25bc6b4a3023baadd36c299a3d26c412ad
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-17 07:18:21 -0700

    Add new get pages

--- lib/Constants/get_pages.dart
+++ lib/Constants/get_pages.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
 import 'package:one_restaurant_delivery/Constants/get_routes.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Private/orders_screen.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Private/shopping_cart_screen.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Public/home_screen.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Public/splash_screen.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Private/meal_screen.dart';
 
 List<GetPage<dynamic>> getPages = [
   GetPage(
@@ -11,5 +14,17 @@ List<GetPage<dynamic>> getPages = [
   GetPage(
     name: AppRoutes.homeScreen,
     page: () => HomeScreen(),
+  ),
+  GetPage(
+    name: AppRoutes.mealScreen,
+    page: () => MealScreen(),
+  ),
+  GetPage(
+    name: AppRoutes.shoppingCartScreen,
+    page: () => ShoppingCartScreen(),
+  ),
+  GetPage(
+    name: AppRoutes.ordersScreen,
+    page: () => OrdersScreen(),
   ),
 ];
