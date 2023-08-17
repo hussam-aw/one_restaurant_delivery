d6396bdfc9ba190553a744da4b623748cf4365eb
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-17 07:21:36 -0700

    Add onTap property to widget

--- lib/PresentationLayer/Widgets/Home/favourite_meal_box.dart
+++ lib/PresentationLayer/Widgets/Home/favourite_meal_box.dart
@@ -6,61 +6,67 @@ import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_ima
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
 
 class FavoutiteMealBox extends StatelessWidget {
-  const FavoutiteMealBox(
-      {super.key,
-      required this.mealImage,
-      required this.mealName,
-      required this.mealPrice});
+  const FavoutiteMealBox({
+    super.key,
+    required this.mealImage,
+    required this.mealName,
+    required this.mealPrice,
+    required this.onTap,
+  });
 
   final String mealImage;
   final String mealName;
   final String mealPrice;
+  final Function() onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170,
-      height: 200,
-      padding: const EdgeInsets.all(10),
-      decoration: const BoxDecoration(
-        borderRadius: raduis20,
-        color: UIColors.lightDeepBlue,
-      ),
-      child: Column(
-        children: [
-          Expanded(
-            child: OrdImageContainer(
-              imagePath: mealImage,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 170,
+        height: 200,
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+          borderRadius: raduis20,
+          color: UIColors.lightDeepBlue,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: OrdImageContainer(
+                imagePath: mealImage,
+              ),
             ),
-          ),
-          spacerHeight(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 6),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Text(
-                    mealName,
-                    softWrap: true,
-                    textAlign: TextAlign.right,
-                    style: UITextStyle.xsmall,
+            spacerHeight(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      mealName,
+                      softWrap: true,
+                      textAlign: TextAlign.right,
+                      style: UITextStyle.xsmall,
+                    ),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    mealPrice,
-                    softWrap: true,
-                    textAlign: TextAlign.left,
-                    style: UITextStyle.xsmall,
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      mealPrice,
+                      softWrap: true,
+                      textAlign: TextAlign.left,
+                      style: UITextStyle.xsmall,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
