0ea80e69cf16c9f0374bfb2a66f8ee6a682d0d50
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-17 07:22:38 -0700

    Add color and border radius properties

--- lib/PresentationLayer/Widgets/Public/ord_image_container.dart
+++ lib/PresentationLayer/Widgets/Public/ord_image_container.dart
@@ -3,16 +3,23 @@ import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
 import 'package:one_restaurant_delivery/Constants/ui_styles.dart';
 
 class OrdImageContainer extends StatelessWidget {
-  const OrdImageContainer({super.key, required this.imagePath});
+  const OrdImageContainer({
+    super.key,
+    required this.imagePath,
+    this.backgroundColor = UIColors.darkGray,
+    this.borderRadius = raduis20,
+  });
 
   final String imagePath;
+  final Color? backgroundColor;
+  final BorderRadius? borderRadius;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: UIColors.darkGray,
-        borderRadius: raduis20,
+        color: backgroundColor,
+        borderRadius: borderRadius,
         image: DecorationImage(
           image: AssetImage(imagePath),
           fit: BoxFit.cover,
