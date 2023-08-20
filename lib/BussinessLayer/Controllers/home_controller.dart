1074f781d005a4074ffd97b6ae219729939a037b
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-20 07:35:44 -0700

    Use controllers to fetch home screen data

--- lib/BussinessLayer/Controllers/home_controller.dart
+++ lib/BussinessLayer/Controllers/home_controller.dart
@@ -1,59 +1,26 @@
 import 'package:get/get.dart';
-import 'package:one_restaurant_delivery/Constants/get_pages.dart';
-import 'package:one_restaurant_delivery/DataAccesslayer/Models/meal.dart';
-import 'package:one_restaurant_delivery/DataAccesslayer/Models/offer.dart';
-
-import '../../Constants/get_routes.dart';
-import '../../DataAccesslayer/Models/Category.dart';
-import '../../DataAccesslayer/Repositories/category_repo.dart';
-import '../../DataAccesslayer/Repositories/meal_repo.dart';
-import '../../DataAccesslayer/Repositories/offer_repo.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/cart_controller.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/categories_controller.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/meals_controller.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/offers_controller.dart';
 
 class HomeController extends GetxController {
-  CategoryRepo categoryRepo = CategoryRepo();
-  MealRepo mealRepo = MealRepo();
-  OfferRepo offerRepo = OfferRepo();
+  var offersController = Get.put(OffersController());
+  var categoriesController = Get.put(CategoriesController());
+  var mealsController = Get.put(MealsController());
+  final cartController = Get.put(CartController());
 
-  List<Category> categories = [];
-  List<Offer> offers = [];
-  List<Meal> meals = [];
-  List<Meal> mealsByCategory = [];
-
-  late int current = 0;
+  void fetchHomeData() async {
+    await offersController.getOffers();
+    await categoriesController.getCategories();
+    await mealsController.getFeaturedMeals();
+    await mealsController.getMeals();
+    await cartController.getCartItems();
+  }
 
   @override
   onInit() async {
-    categories = await categoryRepo.getCategories();
-
-    offers = await offerRepo.getOffers();
-
-    meals = await mealRepo.getFeaturedMeals();
-
-    update();
-
+    fetchHomeData();
     super.onInit();
-  }
-
-  getMeals(categoryId) async {
-    current = categoryId;
-
-    mealsByCategory = meals
-        .where((element) => element.categoryId.isEqual(categoryId))
-        .toList();
-
-    if (mealsByCategory.isNotEmpty) {
-      update();
-    }
-    if (mealsByCategory.isEmpty) {
-      mealsByCategory = meals;
-      update();
-    }
-    if (categoryId == 0) {
-      mealsByCategory = meals;
-      current = 0;
-      update();
-    }
-
-    Get.toNamed(AppRoutes.Meals);
   }
 }
