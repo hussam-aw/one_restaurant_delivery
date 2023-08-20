69698223480e936ecd987b576427dbdf2926af05
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-20 07:37:39 -0700

    Pass cart item and meal and show its details

--- lib/PresentationLayer/Widgets/Private/Cart/cart_item_box.dart
+++ lib/PresentationLayer/Widgets/Private/Cart/cart_item_box.dart
@@ -3,11 +3,16 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
 import 'package:one_restaurant_delivery/Constants/ui_styles.dart';
 import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Models/cart_item.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Models/meal.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_image_container.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
 
 class CartItemBox extends StatelessWidget {
-  const CartItemBox({super.key});
+  const CartItemBox({super.key, required this.cartItem, required this.meal});
+
+  final CartItem cartItem;
+  final Meal meal;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +34,7 @@ class CartItemBox extends StatelessWidget {
                 borderRadius: raduis20,
               ),
               child: OrdImageContainer(
-                imagePath: 'assets/images/co1.png',
+                imagePath: meal.image,
               ),
             ),
           ),
@@ -41,7 +46,7 @@ class CartItemBox extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'وجبة كريسبي مع المقبلات',
+                    meal.name,
                     style: UITextStyle.body,
                   ),
                   spacerHeight(height: 12),
@@ -49,11 +54,12 @@ class CartItemBox extends StatelessWidget {
                     text: TextSpan(
                       style: UITextStyle.xsmall,
                       children: [
-                        TextSpan(text: 'العدد: 5'),
+                        TextSpan(text: 'العدد: ${cartItem.qty}'),
                         TextSpan(text: ' - '),
-                        TextSpan(text: 'الافرادي: 10'),
+                        TextSpan(text: 'الافرادي: ${meal.price}'),
                         TextSpan(text: ' - '),
-                        TextSpan(text: 'الاجمالي: 50\$'),
+                        TextSpan(
+                            text: 'الاجمالي: ${meal.price * cartItem.qty}\$'),
                       ],
                     ),
                   ),
