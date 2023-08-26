da721eed75f24c3df1a422f7c7d82a3faa1ca659
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-26 06:34:20 -0700

    Add height property

--- lib/PresentationLayer/Widgets/Public/ord_image_container.dart
+++ lib/PresentationLayer/Widgets/Public/ord_image_container.dart
@@ -6,17 +6,20 @@ class OrdImageContainer extends StatelessWidget {
   const OrdImageContainer({
     super.key,
     required this.imagePath,
-    this.backgroundColor = UIColors.darkGray,
+    this.height,
+    this.backgroundColor = UIColors.darkGrey,
     this.borderRadius = raduis20,
   });
 
   final String imagePath;
+  final double? height;
   final Color? backgroundColor;
   final BorderRadius? borderRadius;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: height,
       decoration: BoxDecoration(
         color: backgroundColor,
         borderRadius: borderRadius,
