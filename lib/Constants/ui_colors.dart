eb554857303fd194e03c961ec8ad9c0c7f0d5fae
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-26 06:17:36 -0700

    Add shimmer colors

--- lib/Constants/ui_colors.dart
+++ lib/Constants/ui_colors.dart
@@ -5,11 +5,15 @@ class UIColors {
   static const white = Color(0xffffffff);
   static const black = Color(0xFF000000);
   static const red = Color(0xFFE73D47);
-  static const veryDarkGray = Color(0xFF707070);
-  static const darkGray = Color(0xFF979797);
+  static const veryDarkGrey = Color(0xFF707070);
+  static const darkGrey = Color(0xFF979797);
   static const darkDeepBlue = Color(0xFF222433);
   //Light Colors
   static const lightRed = Color(0xFFED6971);
-  static const lightGray = Color(0xFFE2E2E2);
+  static const lightGrey = Color(0xFFE2E2E2);
   static const lightDeepBlue = Color(0xFF32344A);
+
+  static const shimmersBase = Color.fromARGB(255, 184, 184, 184);
+  static const shimmersHighlight = Color.fromARGB(255, 231, 231, 231);
+  static const shimmersContainer = Color.fromARGB(255, 151, 151, 151);
 }
