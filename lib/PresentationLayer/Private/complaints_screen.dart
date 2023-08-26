d86d249a2877476b2696bc296b628c22f36754de
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-26 06:36:19 -0700

    Create complaints screen

---
+++ lib/PresentationLayer/Private/complaints_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_appbar.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_drawer.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/page_title.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
+
+class ComplaintsScreen extends StatelessWidget {
+  const ComplaintsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: ordAppBar(),
+        drawer: const OrdDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          child: Column(
+            children: [
+              const PageTitle(title: 'الشكاوى والمقترحات'),
+              spacerHeight(height: 22),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
