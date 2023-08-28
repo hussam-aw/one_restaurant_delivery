e088ea478bbb20a9d45caa2ddb58f5aac52ed242
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-28 01:11:01 -0700

    Create send complaint bottom sheet

---
+++ lib/PresentationLayer/Widgets/Private/Complaints/send_complaint_bottom_sheet.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/complaint_controller.dart';
+import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
+import 'package:one_restaurant_delivery/Constants/ui_styles.dart';
+import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/accept_button.dart';
+
+class SendComplaintBottomSheet extends StatelessWidget {
+  SendComplaintBottomSheet({super.key});
+
+  final complaintController = Get.put(ComplaintController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: Get.width * .7,
+      decoration: const BoxDecoration(
+        borderRadius: raduis32Top,
+        color: UIColors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 40),
+        child: Column(
+          children: [
+            Expanded(
+              child: Text(
+                'إرسال مقترح | شكوى',
+                style: UITextStyle.body.copyWith(
+                  color: UIColors.lightDeepBlue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: TextFormField(
+                  maxLines: 4,
+                  textAlign: TextAlign.center,
+                  controller:
+                      complaintController.complaintDescriptionController,
+                  style: UITextStyle.body.copyWith(
+                    color: UIColors.darkGrey,
+                  ),
+                  decoration: customTextFieldStyle.copyWith(
+                    fillColor: UIColors.lightGrey,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: AcceptButton(
+                text: 'إرسال',
+                onPressed: () {
+                  complaintController.createCpmplaint();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
