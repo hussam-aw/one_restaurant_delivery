c60ffa0ffa44b4943d816e17ea95fb25e9e95df2
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-28 05:23:42 -0700

    Create place box

---
+++ lib/PresentationLayer/Widgets/Private/Places/place_box.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
+import 'package:one_restaurant_delivery/Constants/ui_styles.dart';
+import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_width.dart';
+
+class PlaceBox extends StatelessWidget {
+  const PlaceBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: const BoxDecoration(
+        borderRadius: raduis22,
+        color: UIColors.lightDeepBlue,
+      ),
+      child: Row(
+        children: [
+          const Icon(
+            FontAwesomeIcons.locationDot,
+            size: 30,
+            color: UIColors.red,
+          ),
+          spacerWidth(width: 22),
+          Expanded(
+            child: Text(
+              'المكتب',
+              style: UITextStyle.body.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          spacerWidth(),
+          InkWell(
+            onTap: () {},
+            child: const Icon(
+              FontAwesomeIcons.minus,
+              size: 35,
+              color: UIColors.darkGrey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
