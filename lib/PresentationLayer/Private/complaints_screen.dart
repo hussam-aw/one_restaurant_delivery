f06306b59f342572e0f1d47eefe1c2cfa4572315
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-28 01:10:48 -0700

    Create complaints screen

--- lib/PresentationLayer/Private/complaints_screen.dart
+++ lib/PresentationLayer/Private/complaints_screen.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/complaints_controller.dart';
+import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
+import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Models/complaint.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Complaints/complaint_box.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Complaints/send_complaint_bottom_sheet.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/loading_item.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_appbar.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_drawer.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/page_title.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
 
 class ComplaintsScreen extends StatelessWidget {
-  const ComplaintsScreen({super.key});
+  ComplaintsScreen({super.key});
+
+  final complaintsController = Get.find<ComplaintsController>();
+
+  Widget buildComplaintsList(List<Complaint> complaints) {
+    return complaints.isEmpty
+        ? const Center(
+            child: Text(
+              'لا يوجد شكاوى',
+              style: UITextStyle.medium,
+            ),
+          )
+        : ListView.separated(
+            itemBuilder: (context, index) {
+              return ComplaintBox(
+                complaint: complaints[index],
+              );
+            },
+            separatorBuilder: (context, index) => spacerHeight(),
+            itemCount: complaints.length,
+          );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +51,25 @@ class ComplaintsScreen extends StatelessWidget {
             children: [
               const PageTitle(title: 'الشكاوى والمقترحات'),
               spacerHeight(height: 22),
+              Expanded(
+                child: Obx(() {
+                  return complaintsController.isLoadingComplaints.value
+                      ? Center(child: loadingItem(width: 100, isWhite: true))
+                      : buildComplaintsList(complaintsController.complaints);
+                }),
+              )
             ],
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(
+            FontAwesomeIcons.plus,
+            color: UIColors.white,
+            size: 30,
+          ),
+          onPressed: () {
+            Get.bottomSheet(SendComplaintBottomSheet());
+          },
         ),
       ),
     );
