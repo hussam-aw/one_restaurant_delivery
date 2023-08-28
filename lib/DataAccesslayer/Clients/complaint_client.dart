87ce85f61e1d47f634d62a5476e8c1683723edbe
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-28 01:07:57 -0700

    Add get complaint and create complaint methods

--- lib/DataAccesslayer/Clients/complaint_client.dart
+++ lib/DataAccesslayer/Clients/complaint_client.dart
@@ -1,8 +1,35 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 import '../../Constants/api_links.dart';
 
 class ComplaintClient {
+  Future<dynamic> getComplaints() async {
+    var response = await http.get(Uri.parse('$baseUrl$complaintsLink/1'));
 
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "";
+    }
+  }
 
+  Future<dynamic> createComplaint(complaintDescription) async {
+    var response = await http.post(Uri.parse('$baseUrl$complaintLink'),
+        body: jsonEncode(<String, dynamic>{
+          "description": complaintDescription,
+          "user_id": 1,
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
 }
--- lib/DataAccesslayer/Repositories/complaint_repo.dart
+++ lib/DataAccesslayer/Repositories/complaint_repo.dart
@@ -1,8 +1,27 @@
 import 'dart:convert';
+
+import 'package:one_restaurant_delivery/DataAccesslayer/Models/complaint.dart';
+
 import '../Clients/complaint_client.dart';
-import '../Models/complaint.dart';
 
 class ComplaintRepo {
-
   ComplaintClient client = ComplaintClient();
+
+  Future<List<Complaint>> getComplaints() async {
+    var response = await client.getComplaints();
+    if (response != "") {
+      final parsed = json.decode(response).cast<Map<String, dynamic>>();
+      return parsed.map<Complaint>((json) => Complaint.fromMap(json)).toList();
+    }
+    return [];
+  }
+
+  Future<Complaint?> createComplaint(complaintDescription) async {
+    var response = await client.createComplaint(complaintDescription);
+    if (response != "") {
+      final parsed = json.decode(response);
+      return Complaint.fromMap(parsed);
+    }
+    return null;
+  }
 }
