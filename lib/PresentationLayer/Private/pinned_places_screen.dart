45e08476eb8fc5d499fad4ac91194965ceb6e266
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-29 05:52:55 -0700

    Update pinned places screen to show pinned places and add new place

--- lib/PresentationLayer/Private/pinned_places_screen.dart
+++ lib/PresentationLayer/Private/pinned_places_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Places/add_curren_location_Bottom_sheet.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/places_controller.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Places/add_location_bottom_sheet.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Places/place_box.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/add_floating_button.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/loading_item.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_appbar.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_drawer.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/page_title.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
 
 class PinnedPlacesScreen extends StatelessWidget {
-  const PinnedPlacesScreen({super.key});
+  PinnedPlacesScreen({super.key});
+
+  final placesController = Get.find<PlacesController>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,20 +30,26 @@ class PinnedPlacesScreen extends StatelessWidget {
               const PageTitle(title: 'الأماكن المثبتة'),
               spacerHeight(),
               Expanded(
-                child: ListView.separated(
-                  itemBuilder: (context, index) {
-                    return PlaceBox();
-                  },
-                  separatorBuilder: (context, index) => spacerHeight(),
-                  itemCount: 6,
-                ),
+                child: Obx(() {
+                  return placesController.isLoadingPinnedPlaces.value
+                      ? Center(child: loadingItem(width: 100, isWhite: true))
+                      : ListView.separated(
+                          itemBuilder: (context, index) {
+                            return PlaceBox(
+                              place: placesController.pinnedPlaces[index],
+                            );
+                          },
+                          separatorBuilder: (context, index) => spacerHeight(),
+                          itemCount: placesController.pinnedPlaces.length,
+                        );
+                }),
               )
             ],
           ),
         ),
         floatingActionButton: AddFloatingButton(
           onPressed: () {
-            Get.bottomSheet(const AddCurrentLocationBottomSheet());
+            Get.bottomSheet(AddCurrentLocationBottomSheet());
           },
         ),
       ),
