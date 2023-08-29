a9936f24c53a21ba2267ef7eb18d553e98eb7b76
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-29 05:53:53 -0700

    Add create order methods

--- lib/DataAccesslayer/Clients/order_client.dart
+++ lib/DataAccesslayer/Clients/order_client.dart
@@ -1,8 +1,29 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-import '../../Constants/api_links.dart';
+import 'package:http/http.dart' as http;
+import 'package:one_restaurant_delivery/Constants/api_links.dart';
 
 class OrderClient {
-
-
+  Future<dynamic> createOrder(
+      details, isDiscount, discount, address, lat, long, notes) async {
+    var response = await http.post(Uri.parse('$baseUrl$orderLink'),
+        body: jsonEncode(<String, dynamic>{
+          "notes": notes,
+          "discount": discount,
+          "is_discount": isDiscount,
+          "address": address,
+          "lat": lat,
+          "long": long,
+          "details": details,
+          "user_id": 1,
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+    if (response.statusCode == 201) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
 }
--- lib/DataAccesslayer/Repositories/order_repo.dart
+++ lib/DataAccesslayer/Repositories/order_repo.dart
@@ -1,9 +1,15 @@
-import 'dart:convert';
-import '../Clients/order_client.dart';
-import '../Models/Order.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Clients/order_client.dart';
 
 class OrderRepo {
-
   OrderClient client = OrderClient();
 
+  Future<bool> createOrder(
+      details, isDiscount, discount, address, lat, long, notes) async {
+    var response = await client.createOrder(
+        details, isDiscount, discount, address, lat, long, notes);
+    if (response != "") {
+      return true;
+    }
+    return false;
   }
+}
