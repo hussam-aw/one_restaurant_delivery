1fcc8f55788d81f8a04a3dd79ff74b07f891d412
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-08-30 01:58:40 -0700

    Add is loading parameter

--- lib/PresentationLayer/Widgets/Public/accept_button.dart
+++ lib/PresentationLayer/Widgets/Public/accept_button.dart
@@ -1,36 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
 import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/loading_item.dart';
 
 // ignore: must_be_immutable
 class AcceptButton extends StatelessWidget {
-  const AcceptButton({
+  AcceptButton({
     super.key,
     required this.text,
     this.backgroundColor = UIColors.red,
     this.textStyle = UITextStyle.medium,
     required this.onPressed,
+    this.isLoading = false,
   });
 
   final String text;
   final Color backgroundColor;
   final TextStyle textStyle;
   final Function()? onPressed;
+  bool isLoading;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: isLoading ? () {} : onPressed,
       style: Theme.of(context).elevatedButtonTheme.style!.copyWith(
             backgroundColor: MaterialStatePropertyAll<Color>(backgroundColor),
           ),
       child: Align(
         alignment: Alignment.center,
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
-          softWrap: true,
-          style: textStyle,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (isLoading) loadingItem(isWhite: true),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              softWrap: true,
+              style: textStyle,
+            ),
+          ],
         ),
       ),
     );
