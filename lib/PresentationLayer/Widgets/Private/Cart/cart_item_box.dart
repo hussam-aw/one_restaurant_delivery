0642491aa3bb897217fa5e2b0b62b2acce894633
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-07 08:55:33 -0700

    Add delete icon button and use update qty and remove for cart item

--- lib/PresentationLayer/Widgets/Private/Cart/cart_item_box.dart
+++ lib/PresentationLayer/Widgets/Private/Cart/cart_item_box.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/cart_controller.dart';
 import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
 import 'package:one_restaurant_delivery/Constants/ui_styles.dart';
 import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
 import 'package:one_restaurant_delivery/DataAccesslayer/Models/cart_item.dart';
 import 'package:one_restaurant_delivery/DataAccesslayer/Models/meal.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Cart/update_qty_bottom_sheet.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_image_container.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
 
 class CartItemBox extends StatelessWidget {
-  const CartItemBox({super.key, required this.cartItem, required this.meal});
+  CartItemBox({super.key, required this.cartItem, required this.meal});
 
   final CartItem cartItem;
   final Meal meal;
+  final cartController = Get.find<CartController>();
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +32,7 @@ class CartItemBox extends StatelessWidget {
           Expanded(
             flex: 1,
             child: Container(
-              height: 67,
+              height: 70,
               decoration: const BoxDecoration(
                 color: UIColors.lightGrey,
                 borderRadius: raduis20,
@@ -41,7 +45,7 @@ class CartItemBox extends StatelessWidget {
           Expanded(
             flex: 3,
             child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 14),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -68,12 +72,29 @@ class CartItemBox extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.solidPenToSquare,
-                size: 25,
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Get.bottomSheet(UpdateQtyBottomSheet(meal: meal));
+                  },
+                  child: const Icon(
+                    FontAwesomeIcons.solidPenToSquare,
+                    size: 22,
+                  ),
+                ),
+                spacerHeight(),
+                InkWell(
+                  onTap: () {
+                    cartController.removeFromCart(meal.id);
+                  },
+                  child: const Icon(
+                    Icons.delete,
+                    size: 28,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
