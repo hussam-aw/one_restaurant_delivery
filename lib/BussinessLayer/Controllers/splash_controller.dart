447ad26babb304b6ca3e814636da81fb460a9592
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-09 07:22:17 -0700

    Add request current location and get initial settings method

--- lib/BussinessLayer/Controllers/splash_controller.dart
+++ lib/BussinessLayer/Controllers/splash_controller.dart
@@ -1,11 +1,36 @@
 import 'package:get/get.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Helpers/location_helper.dart';
 import 'package:one_restaurant_delivery/Constants/get_routes.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Models/coordinates.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Repositories/settings_repo.dart';
+import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Splash/request_location_dialog.dart';
+import 'package:one_restaurant_delivery/main.dart';
 
 class SplashController extends GetxController {
+  LocationHelper locationHelper = LocationHelper();
+  Coordinates? locationData;
+  SettingsRepo repo = SettingsRepo();
+
+  Future<void> requestCurrentLocation() async {
+    locationData = await locationHelper.getCurrentLocation();
+    if (locationData != null) {
+      Future.delayed(const Duration(seconds: 3)).then((value) {
+        MyApp.currentLocationData = locationData;
+        Get.offAndToNamed(AppRoutes.homeScreen);
+      });
+    } else {
+      Get.dialog(barrierDismissible: false, RequestLocationDialog());
+    }
+  }
+
+  Future<void> getInitialSettings() async {
+    MyApp.initialSettings = await repo.getInitialSettings();
+  }
+
   @override
   void onInit() async {
-    Future.delayed(const Duration(seconds: 3))
-        .then((value) => Get.offAndToNamed(AppRoutes.homeScreen));
+    await requestCurrentLocation();
+    await getInitialSettings();
     super.onInit();
   }
 }
