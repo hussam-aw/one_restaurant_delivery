3c3486319153174bb37e121a697e2dc8850e9664
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-09 07:21:25 -0700

    Update create place method

--- lib/BussinessLayer/Controllers/place_controller.dart
+++ lib/BussinessLayer/Controllers/place_controller.dart
@@ -1,33 +1,30 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:one_restaurant_delivery/BussinessLayer/Controllers/places_controller.dart';
-import 'package:one_restaurant_delivery/BussinessLayer/Helpers/location_helper.dart';
 import 'package:one_restaurant_delivery/DataAccesslayer/Clients/box_client.dart';
 import 'package:one_restaurant_delivery/DataAccesslayer/Models/place.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/snackbars.dart';
+import 'package:one_restaurant_delivery/main.dart';
 
 class PlaceController extends GetxController {
-  LocationHelper locationHelper = LocationHelper();
-  Map<String, double?>? currentLocationData;
   TextEditingController placeNameController = TextEditingController();
   BoxClient boxClient = BoxClient();
   final placesController = Get.find<PlacesController>();
-  var loadingLocation = false.obs;
+  //var loadingLocation = false.obs;
   var savingPlace = false.obs;
 
-  Future<void> getCurrentLocationData() async {
-    loadingLocation.value = true;
-    currentLocationData = await locationHelper.getCurrentLocation();
-    loadingLocation.value = false;
-  }
+  // Future<void> getCurrentLocationData() async {
+  //   loadingLocation.value = true;
+  //   currentLocationData = await locationHelper.getCurrentLocation();
+  //   loadingLocation.value = false;
+  // }
 
   Future<void> createPlace() async {
     if (placeNameController.text.isNotEmpty) {
       savingPlace.value = true;
       Place place = Place(
         name: placeNameController.text,
-        lat: currentLocationData!['lat'],
-        long: currentLocationData!['long'],
+        coordinates: MyApp.currentLocationData!,
       );
       placesController.pinnedPlaces.add(place);
       await boxClient.addToPinnedPlaces(placesController.pinnedPlaces);
