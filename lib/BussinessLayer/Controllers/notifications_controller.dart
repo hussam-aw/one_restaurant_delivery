475545c2063cdb7b497f7a9cd5357cc6adf77647
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-10 03:20:58 -0700

    Create get notifications method

--- lib/BussinessLayer/Controllers/notifications_controller.dart
+++ lib/BussinessLayer/Controllers/notifications_controller.dart
@@ -1,3 +1,13 @@
 import 'package:get/get.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Models/user_notification.dart';
+import 'package:one_restaurant_delivery/DataAccesslayer/Repositories/notification_repo.dart';
 
-class NotificationsController extends GetxController {}
+class NotificationsController extends GetxController {
+  NotificationRepo repo = NotificationRepo();
+  List<UserNotification> notifications = [];
+
+  Future<void> getNotifications() async {
+    notifications = await repo.getNotifications();
+    update();
+  }
+}
