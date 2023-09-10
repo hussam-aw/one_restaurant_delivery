fd82446b59b68781b918f22da3617a2f762b24b9
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-10 03:28:33 -0700

    Add circle shape property

--- lib/PresentationLayer/Widgets/Public/ord_icon_button.dart
+++ lib/PresentationLayer/Widgets/Public/ord_icon_button.dart
@@ -8,24 +8,31 @@ class OrdIconButton extends StatelessWidget {
     required this.icon,
     this.backgroundColor = UIColors.white,
     this.borderRadius = raduis12,
+    this.circleShape = false,
     required this.onPressed,
   });
 
   final Icon icon;
   final Color backgroundColor;
   final BorderRadius borderRadius;
+  final bool circleShape;
   final Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: borderRadius,
-        border: Border.all(
-          color: UIColors.veryDarkGrey,
-        ),
-      ),
+      decoration: circleShape
+          ? ShapeDecoration(
+              color: backgroundColor,
+              shape: const CircleBorder(),
+            )
+          : BoxDecoration(
+              color: backgroundColor,
+              borderRadius: borderRadius,
+              border: Border.all(
+                color: UIColors.veryDarkGrey,
+              ),
+            ),
       child: Center(
         child: IconButton(onPressed: onPressed, icon: icon),
       ),
