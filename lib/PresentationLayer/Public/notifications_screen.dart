8d9fc163040c25ca1d2228985ddde8a00c0fc857
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-10 03:26:04 -0700

    Update screen to display notifications list

--- lib/PresentationLayer/Public/notifications_screen.dart
+++ lib/PresentationLayer/Public/notifications_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/notifications_controller.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Private/Notifications/notification_box.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_appbar.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_drawer.dart';
@@ -6,7 +8,9 @@ import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/page_ti
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
 
 class NotificationsScreen extends StatelessWidget {
-  const NotificationsScreen({super.key});
+  NotificationsScreen({super.key});
+
+  final notificationsController = Get.find<NotificationsController>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +26,17 @@ class NotificationsScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const PageTitle(title: 'الإشعارات'),
-              // Expanded(
-              //     child: ListView.separated(
-              //   itemBuilder: (context,index) {
-              //     return NotificationBox(notification: notification)
-              //   },
-              //   separatorBuilder: (context,index) => spacerHeight(),
-              //   itemCount: 5,
-              // )),
+              spacerHeight(),
+              Expanded(
+                  child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return NotificationBox(
+                      notification:
+                          notificationsController.notifications[index]);
+                },
+                separatorBuilder: (context, index) => spacerHeight(),
+                itemCount: notificationsController.notifications.length,
+              )),
             ],
           ),
         ),
