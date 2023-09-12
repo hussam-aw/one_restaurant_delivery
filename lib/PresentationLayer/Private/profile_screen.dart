18c1d29e350819ec336e3ad45d618c963a32f401
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-12 04:23:13 -0700

    Make screen dynamic to update profile

--- lib/PresentationLayer/Private/profile_screen.dart
+++ lib/PresentationLayer/Private/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Controllers/profile_controller.dart';
 import 'package:one_restaurant_delivery/Constants/ui_colors.dart';
 import 'package:one_restaurant_delivery/Constants/ui_text_styles.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/accept_button.dart';
@@ -12,9 +13,12 @@ import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/ord_tex
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/page_title.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/section_title.dart';
 import 'package:one_restaurant_delivery/PresentationLayer/Widgets/Public/spacer_height.dart';
+import 'package:one_restaurant_delivery/main.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  ProfileScreen({super.key});
+
+  final profileController = Get.find<ProfileController>();
 
   @override
   Widget build(BuildContext context) {
@@ -32,97 +36,108 @@ class ProfileScreen extends StatelessWidget {
               Expanded(
                 flex: 8,
                 child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Container(
-                        width: Get.width,
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 120,
-                              width: 120,
-                              child: Stack(
-                                clipBehavior: Clip.none,
-                                fit: StackFit.expand,
-                                children: [
-                                  const CircleAvatar(
-                                    backgroundImage: AssetImage(
-                                      'assets/images/person.png',
-                                    ),
-                                  ),
-                                  Positioned(
-                                    bottom: -10,
-                                    left: 0,
-                                    right: -65,
-                                    child: OrdIconButton(
-                                      onPressed: () {},
-                                      circleShape: true,
-                                      icon: const Icon(
-                                        FontAwesomeIcons.solidPenToSquare,
-                                        color: UIColors.darkDeepBlue,
-                                        size: 18,
+                  child: GetBuilder<ProfileController>(
+                    init: profileController,
+                    builder: (_) => Column(
+                      children: [
+                        Container(
+                          width: Get.width,
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 120,
+                                width: 120,
+                                child: Stack(
+                                  clipBehavior: Clip.none,
+                                  fit: StackFit.expand,
+                                  children: [
+                                    const CircleAvatar(
+                                      backgroundImage: AssetImage(
+                                        'assets/images/person.png',
                                       ),
                                     ),
-                                  ),
-                                ],
+                                    Positioned(
+                                      bottom: -10,
+                                      left: 0,
+                                      right: -65,
+                                      child: OrdIconButton(
+                                        onPressed: () {},
+                                        circleShape: true,
+                                        icon: const Icon(
+                                          FontAwesomeIcons.solidPenToSquare,
+                                          color: UIColors.darkDeepBlue,
+                                          size: 18,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
+                              spacerHeight(height: 22),
+                              Text(
+                                MyApp.appUser!.name,
+                                softWrap: true,
+                                style: UITextStyle.heading.copyWith(
+                                  color: UIColors.lightGrey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SectionTitle(title: 'الاسم'),
+                            spacerHeight(),
+                            OrdTextFormField(
+                              controller: profileController.userNameController,
+                              hintText: 'أدخل اسم المستخدم',
                             ),
                             spacerHeight(height: 22),
-                            Text(
-                              'Ali',
-                              softWrap: true,
-                              style: UITextStyle.heading.copyWith(
-                                color: UIColors.lightGrey,
-                              ),
+                            const SectionTitle(title: 'الايميل'),
+                            spacerHeight(),
+                            OrdTextFormField(
+                              controller: profileController.emailController,
+                              hintText: 'أدخل الايميل',
+                            ),
+                            spacerHeight(height: 22),
+                            const SectionTitle(title: 'رقم الموبايل'),
+                            spacerHeight(),
+                            OrdTextFormField(
+                              controller:
+                                  profileController.mobileNumberController,
+                              keyboardType: TextInputType.number,
+                              hintText: 'أدخل رقم الموبايل',
+                              formatters: [
+                                FilteringTextInputFormatter.allow(
+                                    RegExp(r'^\d+\.?\d{0,2}'))
+                              ],
+                            ),
+                            spacerHeight(height: 22),
+                            const SectionTitle(title: 'العنوان'),
+                            spacerHeight(),
+                            OrdTextFormField(
+                              controller: profileController.addressController,
+                              hintText: 'أدخل العنوان',
                             ),
                           ],
                         ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SectionTitle(title: 'الاسم'),
-                          spacerHeight(),
-                          OrdTextFormField(
-                            hintText: 'أدخل اسم المستخدم',
-                          ),
-                          spacerHeight(height: 22),
-                          const SectionTitle(title: 'الايميل'),
-                          spacerHeight(),
-                          OrdTextFormField(
-                            hintText: 'أدخل الايميل',
-                          ),
-                          spacerHeight(height: 22),
-                          const SectionTitle(title: 'رقم الموبايل'),
-                          spacerHeight(),
-                          OrdTextFormField(
-                            keyboardType: TextInputType.number,
-                            hintText: 'أدخل رقم الموبايل',
-                            formatters: [
-                              FilteringTextInputFormatter.allow(
-                                  RegExp(r'^\d+\.?\d{0,2}'))
-                            ],
-                          ),
-                          spacerHeight(height: 22),
-                          const SectionTitle(title: 'العنوان'),
-                          spacerHeight(),
-                          OrdTextFormField(
-                            hintText: 'أدخل العنوان',
-                          ),
-                        ],
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
               spacerHeight(),
-              Expanded(
-                child: AcceptButton(
+              Obx(() {
+                return AcceptButton(
                   text: 'تعديل البيانات',
-                  onPressed: () {},
-                ),
-              ),
+                  onPressed: () {
+                    profileController.updateProfile();
+                  },
+                  isLoading: profileController.isLoading.value,
+                );
+              }),
             ],
           ),
         ),
