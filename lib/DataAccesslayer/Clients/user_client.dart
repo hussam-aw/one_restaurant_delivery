19f86c72c947df3594515a8325c3b331c014e246
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-09-17 02:48:38 -0700

    Update login method and add update user info method

--- lib/DataAccesslayer/Clients/user_client.dart
+++ lib/DataAccesslayer/Clients/user_client.dart
@@ -13,7 +13,7 @@ class UserClient {
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         });
-
+    print(response.body);
     if (response.statusCode == 200) {
       return response.body;
     } else {
@@ -24,27 +24,7 @@ class UserClient {
   Future<dynamic> login(phoneNumber) async {
     var response = await http.post(Uri.parse("$baseUrl$loginLink"),
         body: jsonEncode(<String, dynamic>{
-          "phone": phoneNumber,
-        }),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        });
-
-    if (response.statusCode == 201) {
-      return response.body;
-    } else {
-      return null;
-    }
-  }
-
-  Future<dynamic> updateUserInfo(userName, email, mobilePhone, address) async {
-    var response = await http.post(Uri.parse('$baseUrl$updateProfileLink'),
-        body: jsonEncode(<String, dynamic>{
-          "name": userName,
-          "email": email,
-          "phone": mobilePhone,
-          "address": address,
-          "user_id": MyApp.appUser!.id,
+          "email": phoneNumber,
         }),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
@@ -52,6 +32,30 @@ class UserClient {
     print(response.body);
     if (response.statusCode == 201) {
       return response.body;
+    } else {
+      return null;
+    }
+  }
+
+  Future<dynamic> updateUserInfo(userName, mobilePhone, address, avatar) async {
+    print(mobilePhone);
+    var request =
+        http.MultipartRequest('POST', Uri.parse('$baseUrl$updateProfileLink'));
+    request.fields.addAll({
+      'name': userName.toString(),
+      'phone': mobilePhone.toString(),
+      'address': address.toString(),
+      'user_id': MyApp.appUser!.id.toString(),
+    });
+
+    if (avatar.isNotEmpty) {
+      request.files.add(await http.MultipartFile.fromPath('avatar', avatar));
+    }
+
+    http.StreamedResponse response = await request.send();
+    print(await response.stream.bytesToString());
+    if (response.statusCode == 201) {
+      return await response.stream.bytesToString();
     } else {
       return null;
     }
