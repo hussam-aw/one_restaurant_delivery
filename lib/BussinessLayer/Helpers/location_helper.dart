0210c932e6ded5660f6616b3bbc05fdcd357b27c
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-07 14:33:33 -0700

    Fix getting location

--- lib/BussinessLayer/Helpers/location_helper.dart
+++ lib/BussinessLayer/Helpers/location_helper.dart
@@ -6,21 +6,22 @@ class LocationHelper {
 
   Future<Coordinates?> getCurrentLocation() async {
     PermissionStatus permissionGranted;
-    LocationData locationData;
-
+    LocationData? locationData;
     bool serviceEnabled = await location.serviceEnabled();
     if (!serviceEnabled) {
       serviceEnabled = await location.requestService();
       if (!serviceEnabled) return null;
     }
-
     permissionGranted = await location.hasPermission();
     if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
       if (permissionGranted != PermissionStatus.granted) return null;
     }
-
-    locationData = await location.getLocation();
+    locationData = await Future.any([
+      location.getLocation(),
+      Future.delayed(const Duration(seconds: 3), () => null),
+    ]);
+    locationData ??= await location.getLocation();
     return Coordinates(
         lat: locationData.latitude, long: locationData.longitude);
   }
