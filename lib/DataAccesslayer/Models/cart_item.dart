81e60232cabf5bc7a50f3654074d58ab8e3daa1a
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-08 14:31:48 -0700

    Add price parameter and refactor names

--- lib/DataAccesslayer/Models/cart_item.dart
+++ lib/DataAccesslayer/Models/cart_item.dart
@@ -1,26 +1,30 @@
 class CartItem {
   int mealId;
   int qty;
+  num price;
   num total;
   String? specialOrder;
 
   CartItem({
     required this.mealId,
     required this.qty,
+    required this.price,
     required this.total,
     required this.specialOrder,
   });
 
   factory CartItem.fromJson(Map<String, dynamic> json) => CartItem(
-        mealId: json["id"],
+        mealId: json["meal"],
         qty: json["quantity"],
+        price: json["price"],
         total: json['total_price'],
         specialOrder: json["special_order"] ?? "",
       );
 
   Map<String, dynamic> toJson() => {
-        "id": mealId,
+        "meal": mealId,
         "quantity": qty,
+        "price": price,
         "total_price": total,
         "special_order": specialOrder,
       };
