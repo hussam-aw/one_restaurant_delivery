c5c60bd45d7b094ccf20b598fc3eebc8ba6ef9e7
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-16 00:51:05 -0700

    Initialize firebase app and local notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Helpers/firebase_helper.dart';
+import 'package:one_restaurant_delivery/BussinessLayer/Helpers/local_notification_helper.dart';
 import 'package:one_restaurant_delivery/Constants/get_pages.dart';
 import 'package:one_restaurant_delivery/Constants/ui_themes.dart';
 import 'package:one_restaurant_delivery/DataAccesslayer/Models/coordinates.dart';
@@ -8,6 +11,10 @@ import 'package:one_restaurant_delivery/DataAccesslayer/Models/initial_settings.
 import 'package:one_restaurant_delivery/DataAccesslayer/Models/user.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LocalNotificationHelper.initialize();
+  await Firebase.initializeApp();
+  FirebaseMessagingHelper.initialize();
   await GetStorage.init();
   runApp(const MyApp());
 }
